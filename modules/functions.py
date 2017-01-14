2bdfe913a956e4ee8675d96bb2619edc55d5f5fa
Kristoffer Sahlin <kxs624@psu.edu>
2017-01-13 19:49:39 -0500

    added function to convert positioned query alignments to target into a alignment matrix without indels

--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,22 @@
+"""
+    position_query_to_alignment(): arranges all query alignments onto a target sequence
+    get_non_overlapping_intervals(): finds canonical intervals where a set of reads maps, 
+            A canonic interval assures the same muber of reads are aligned to the target in that interval.
+    create_multialignment_format(): arranges all query alignments that covers the region [start,stop] into a multialignment matrix
+                    useful for statistical testing of SNVs
+
+    minimizer_graph(S): creates the minimizer graph defined in..
+    mmembership_graph(S): creates the membership graph defined in..
+
+
+    FUTURE:
+    identify_allele_regions(): takes a start and stop coordinate and returns positions subject to phasing (two variants have high coverage) 
+    this is useful for phasing haplotypes or SNVs within viral sequences or haplotypes
+
+"""
+
 import unittest
+
 
 def position_query_to_alignment(query_aligned, target_aligned, target_alignment_start_position):
     """
@@ -108,6 +126,44 @@ def get_non_overlapping_intervals(ranges):
     return(non_overlapping_parts)
 
 
+def create_multialignment_format(query_to_target_positioned_dict, start, stop):
+    """
+        only create multialignment format of the query sequences that cover the region [start,stop]
+    """
+
+    # get allreads alignments covering interesting segment
+    # row_accessions = []
+    segments = {}
+    for q_acc, (q_to_t_pos, t_vector_start, t_vector_end) in query_to_target_positioned_dict.items():
+        if t_vector_start <= start and t_vector_end >= stop: # read cover region
+            segment = q_to_t_pos[start - t_vector_start : stop - t_vector_start +1 ]
+            # row_accessions.append(q_acc)
+            segments[q_acc] = segment
+
+
+    # create alignment matrix of segment
+    alignment_matrix = {}
+    for q_acc in segments:
+        alignment_matrix[q_acc] = []
+
+    for j in range(0, stop - start + 1):
+        if (start + j) % 2 == 0:  # we are between a target base pairs (need to check the longest one)
+            insertions = [(segments[q_acc][j], q_acc) for q_acc in segments]
+            max_insertion, q_acc_max_ins = max(insertions, key= lambda x : len(x[0]))
+            for q_acc in segments:
+                for p in range(len(max_insertion)):
+                    # all shorter insertions are left shifted -- identical indels are guaranteed to be aligned
+                    # however, no multialignment is performed among the indels
+                    if p < len(segments[q_acc][j]):
+                        alignment_matrix[q_acc].append(segments[q_acc][j][p])
+                    else:
+                        alignment_matrix[q_acc].append("-")
+        else: # we are on a target base pair -- all varinats must be exactly A,C,G,T, - i.e., length 1
+            for q_acc in segments:
+                alignment_matrix[q_acc].append(segments[q_acc][j])
+    # print(alignment_matrix)
+    return alignment_matrix
+
 
 
 class TestFunctions(unittest.TestCase):
@@ -119,8 +175,8 @@ class TestFunctions(unittest.TestCase):
         """
         t_seq = "ACGGA"
 
-        q, t, t_start = "ACGGA", "ACGGA", 0
-        self.assertEqual(position_query_to_alignment(q, t, t_start), (["-", "A","-", "C", "-","G","-", "G", "-", "A", "-"], 0, 10) )
+        q1, t, t_start = "ACGGA", "ACGGA", 0
+        self.assertEqual(position_query_to_alignment(q1, t, t_start), (["-", "A","-", "C", "-","G","-", "G", "-", "A", "-"], 0, 10) )
 
         q, t, t_start = "TACGGA", "-ACGGA", 0
         self.assertEqual(position_query_to_alignment(q, t, t_start), (["T", "A","-", "C", "-","G","-", "G", "-", "A", "-"], 0, 10) )
@@ -134,12 +190,36 @@ class TestFunctions(unittest.TestCase):
         q, t, t_start = "GGA", "GGA", 2
         self.assertEqual(position_query_to_alignment(q, t, t_start), (["-","G","-", "G", "-", "A", "-"], 4, 10) )
 
+        q, t, t_start = "ACGGCC-", "ACGG--A", 0
+        self.assertEqual(position_query_to_alignment(q, t, t_start), (["-", "A","-", "C", "-","G","-", "G", "CC", "-", "-"], 0, 10) )
+
+        q, t, t_start = "ACGGCC", "ACGG--", 0
+        self.assertEqual(position_query_to_alignment(q, t, t_start), (["-", "A","-", "C", "-","G","-", "G", "CC"], 0, 8) )
+
     def test_intervals(self):
         ranges = [(0,100,'a'),(0,75,'b'),(95,150,'c'),(120,130,'d')]
         result = [(0,75,("a", "b")),(76, 94, ("a",)), (95,100, ("a", "c")), (101,119, ("c",)), (120,130,("c","d")), (131,150,("c",)) ]
         self.assertEqual(get_non_overlapping_intervals(ranges), result)
 
+    def test_alignment_matrix(self):
+        self.maxDiff = None
+        start, stop = 0, 10
+        query_to_target_positioned_dict = {"q1" : ( ["-", "A","-", "C", "-", "G", "ACCG",   "G", "-", "A", "TTT"], 0, 10),
+                                            "q2" : (["-", "A","-", "C", "-", "G", "AG",     "G", "-", "A", "TTT"], 0, 10),
+                                            "q3" : (["-", "A","-", "C", "-", "G", "A",      "G", "-", "A", "TTT"], 0, 10),
+                                            "q4" : (["-", "A","-", "C", "-", "G", "CC",     "G", "-", "A", "-"], 0, 10),
+                                            "q5" : (["-", "A","-", "C", "-", "G", "-",      "G", "-", "A", "T"], 0, 10),
+                                            "q6" : (["G", "A","-", "C", "-", "G", "C",      "G", "-", "A", "-"], 0, 10)}
 
+        alignment_matrix = {"q1" : ["-", "A","-", "C", "-", "G","A","C","C","G", "G", "-", "A", "T","T","T"],
+                            "q2" : ["-", "A","-", "C", "-", "G","A","G","-","-", "G", "-", "A", "T","T","T"],
+                            "q3" : ["-", "A","-", "C", "-", "G","A","-","-","-", "G", "-", "A", "T","T","T"],
+                            "q4" : ["-", "A","-", "C", "-", "G","C","C","-","-", "G", "-", "A", "-","-","-"],
+                            "q5" : ["-", "A","-", "C", "-", "G","-","-","-","-", "G", "-", "A", "T","-","-"],
+                            "q6" : ["G", "A","-", "C", "-", "G","C","-","-","-", "G", "-", "A", "-","-","-"]
+                            }
+
+        self.assertEqual(create_multialignment_format(query_to_target_positioned_dict, start, stop), alignment_matrix )
 
 if __name__ == '__main__':
     unittest.main()
